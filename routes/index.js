2403cfbd1d7fd4179c876c6a24de9562e2cd9e0a
tyf914 <914635261@qq.com>
2017-10-23 18:20:52 +0800

    Add homepage.hbs & edit related files

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,7 @@
 let fn_index = async (ctx, next) => {
-    ctx.response.type = 'text/html';
-    ctx.response.body = `<p>index page</p>`;
-};
-
-let fn_signin = async (ctx, next) => {
-    ctx.response.type = 'text/html';
-    ctx.response.body = `<p>signin page</p>`;
+    await ctx.render('homepage');
 };
 
 module.exports = {
-    'GET /': fn_index,
-    'POST /signin': fn_signin
+    'GET /': fn_index
 };
